716661a4d674296441b4930dd510f87643ddb810
Davron <davronbekq467@gmail.com>
2024-09-25 14:52:27 +0500

    fix: firebase

--- server/firebase.js
+++ server/firebase.js
@@ -1,9 +1,14 @@
 // firebase.js
 const admin = require("firebase-admin");
-const serviceAccount = require("./firebase-adminsdk.json");
+require("dotenv").config();
 
 admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
+  credential: admin.credential.cert({
+    projectId: process.env.FIREBASE_PROJECT_ID,
+    privateKey: process.env.FIREBASE_PRIVATE_KEY, // Ensure newline characters are handled properly
+    clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+  }),
+  databaseURL: "https://your-project-id.firebaseio.com",
 });
 
 const db = admin.firestore();
